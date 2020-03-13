ffc8d6e36deaabb41375a0a2a9d3a2be71473126
Hari MS <hari7190@gmail.com>
2020-03-13 19:13:37 -0400

    Stock administration back end - Initial commit

---
+++ StockAdministration/sa-backend/StockAdministrationBackEnd/src/main/java/stream/arrays/stockmarket/StockAdministrationBackEnd/StockAdministrationBackEndApplication.java
@@ -0,0 +1,13 @@
+package stream.arrays.stockmarket.StockAdministrationBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAdministrationBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAdministrationBackEndApplication.class, args);
+	}
+
+}
---
+++ StockAdministration/sa-backend/StockAdministrationBackEnd/src/test/java/stream/arrays/stockmarket/StockAdministrationBackEnd/StockAdministrationBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package stream.arrays.stockmarket.StockAdministrationBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAdministrationBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
